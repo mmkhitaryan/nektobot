f15d3ea64e28d4239fc3760357dca2417d2ff9d2
Marat Mkhitaryan <marataziat@gmail.com>
2021-11-03 16:04:28 +0600

    меньше ошибок

--- ws.py
+++ ws.py
@@ -18,9 +18,14 @@ class NektoRoulette():
         return f'{user_id}_{getTime}'
 
     async def pinger(self):
-        while True:
-            await self.websocket.send("2")
-            await asyncio.sleep(20)
+        try:
+            while True:
+                if self.websocket.open:
+                    await self.websocket.send("2")
+                    await asyncio.sleep(20)
+        except (websockets.exceptions.ConnectionClosedError,
+                asyncio.exceptions.IncompleteReadError):
+            return
 
     async def leave_dialog(self):
         leave_message = '42["action",{"action":"anon.leaveDialog","dialogId":'+self.dialog_id+'}]'
@@ -64,6 +69,7 @@ class NektoRoulette():
                     print("Каптчу выбило блять")
                     # solve 42["action",{"action":"captcha.verify","solution":"03AGdBq2607oBhoB9O1IdZt5oqM2gDU5MxpfDYD7LFV7dDI7HFqe-tjPWD9-9bPHgyxuduvZysI0u6GbBw6VYgZgMPhhJ3NiRQE6fX6mmrXBOuuydlp10gRyMyf1zKcZJhGe-tzJJHGy47p8cl7xAHOOV4l_fud1709tNOVIMTTHXmc6X8EFYgs9kkxn8L11S7pyT4ssm5IV7UfkliPxoisAUJV4bTvNHbrHagUMOSS9Q5Xtqchuwf2sFztCBcQVVq4j2-nBvmsHXsWuGZaQaQr1gNxlTx7_7oxSkSsHxFx0mTr9cpXjjg7Rh_7IfrDzeYZNQlq2kNEoPubc9PeKfVdoiMkCp1H8z02yngnnC1wYbtlsNh8Vt5hgw47jKoCUgcEq1OMQ-lyDCo2dbINtnwBLrJ8pV0dqCo6rwQULN6ryRrmY6UoAaJA1WF4gHSthqfq1GtCzXibhMd","hard":false}]
                     await self.websocket.close()
+                    await self.websocket.wait_closed()
 
             resp = await self.websocket.recv()
             self.dialog_id = str(json.loads(resp[2:])[1]["data"]["id"])
@@ -87,6 +93,7 @@ class NektoRoulette():
                             print(f"Диалог закрыт ливаю")
                             await self.who_to_send_instance.leave_dialog()
                             await self.websocket.close()
+                            await self.websocket.wait_closed()
                     
                         if message_text := data_message.get("message"):
                             if self.id_user!=data_message.get("senderId"):
@@ -106,17 +113,6 @@ class NektoRoulette():
                                 '42["action",{"action":"dialog.setTyping","dialogId":' + self.dialog_id + ',"typing":false}]'
                                 )
 
-                                if False:
-                                    if answer_text:
-                                        print(answer_text)
-                                        if answer_text=='пок':
-                                            leave_message = '42["action",{"action":"anon.leaveDialog","dialogId":'+dialog_id+'}]'
-                                            await self.websocket.send(leave_message)
-                                            print("Послал нах")
-                                            break
-                                        else:
-                                            answer_message = '42["action",{"action":"anon.message","dialogId":'+dialog_id+',"message":"' + answer_text + '","randomId":"'+self.get_message_id(id_user)+'"}]'
-                                            await self.websocket.send(answer_message)
 
 async def hello():
     first_nekto_client = NektoRoulette('marat', "e94eee7259b432d4fccb599a0c1a10aac2b22836ad00dd82e4240af337ce8505")
